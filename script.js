f7d0a8da7bbf551a8b368fa22837745e004f7ec2
worldal <al05.lebreton@gmail.com>
2024-12-03 18:00:36 +0100

    v3

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 window.onload = function () {
   // Ouvrir Twitch
-  window.location.href = "twitch://open";
+  window.location.href = "twitch://open/channel/world_al";
 
   // Ouvrir YouTube (avec un délai pour laisser le temps à l'application Twitch de s'ouvrir)
   setTimeout(function () {
     window.location.href = "vnd.youtube://watch?v=VIDEO_ID"; // Remplacez VIDEO_ID par l'ID de votre vidéo
-  }, 2000); // Délai de 2 secondes
+  }, 5000); // Délai de 2 secondes
 };
